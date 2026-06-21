0873df19da58695ad9a74868607d0475d7e3641c
chriko3 <christoph.kohout03@gmail.com>
2026-06-21 19:24:04 +0200

    feat(add-ingredients): implement add ingredients component with styles and layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuBarComponent } from './components/menu-bar-component/menu-bar-component';
+import { AddIngredientsComponent } from './components/add-ingredients-component/add-ingredients-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuBarComponent],
+  imports: [RouterOutlet, MenuBarComponent, AddIngredientsComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
